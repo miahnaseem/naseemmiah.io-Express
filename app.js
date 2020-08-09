25b997a26d77b73201ecb194a9dbf2b8acacbd69
miahnaseem <miahnaseem@gmail.com>
2020-08-09 19:31:06 -0400

    local hosting

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const app = express();
-app.use(bodyParser.json());
-app.use('/api/events', events.API);
-app.use('/api/forms', forms);
+
+// app.use(bodyParser.json());
+// app.use('/api/events', events.API);
+// app.use('/api/forms', forms);
+
+app.set('view engine', 'pug');
 
 app.get('/', (req, res) => {
-  res.send('Hello, World!');
+  res.render('index');
 });
 
 app.listen(3000, () => {
