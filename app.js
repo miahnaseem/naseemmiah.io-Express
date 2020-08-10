c65705458b6e8d3dc942049386b88b074b767b31
miahnaseem <miahnaseem@gmail.com>
2020-08-10 15:41:22 -0400

    Added all routes

--- app.js
+++ app.js
@@ -11,6 +11,25 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
+
+app.get('/home', (req, res) => {
+  res.render('index');
+ });
+
+app.get('/portfolio', (req, res) => {
+  res.render('portfolio');
+ });
+
+ app.get('/contact', (req, res) => {
+  res.render('contact');
+ });
+ 
+ app.get('/about', (req, res) => {
+  res.render('about');
+ });
+
+
+
 app.listen(3000, () => {
   console.log('Listening on port 3000...');
 });
